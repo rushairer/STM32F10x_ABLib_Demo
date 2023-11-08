f551d27d0f2e34eff7fc3e488adc26d07a8a8fa7
Abenx <5195693+rushairer@users.noreply.github.com>
2023-11-08 11:14:23 +0800

    feat: add key4x4

--- src/main.c
+++ src/main.c
@@ -4,6 +4,8 @@
 #include "stm32f10x_abl_key.h"
 #include "stm32f10x_abl_oled.h"
 #include "stm32f10x_abl_servo.h"
+#include "stm32f10x_abl_pca9685.h"
+#include "stm32f10x_abl_key4x4.h"
 
 const uint8_t OLED_BMP1[] =
     {
@@ -75,11 +77,24 @@ const uint8_t OLED_BMP1[] =
 
 int main()
 {
-    LED_InitTypeDef led1;
-    Led1_Init(&led1);
+    // LED_InitTypeDef led1;
+    // Led1_Init(&led1);
 
-    KEY_InitTypeDef key1;
-    KEY_Init(&key1, RCC_APB2Periph_GPIOB, GPIOB, GPIO_Pin_12);
+    KEY4X4_InitTypeDef keyboard;
+    KEY4X4_Init(
+        &keyboard,
+        RCC_APB2Periph_GPIOB,
+        GPIOB,
+        GPIO_Pin_12,
+        GPIO_Pin_13,
+        GPIO_Pin_14,
+        GPIO_Pin_15,
+        RCC_APB2Periph_GPIOA,
+        GPIOA,
+        GPIO_Pin_7,
+        GPIO_Pin_6,
+        GPIO_Pin_5,
+        GPIO_Pin_4);
 
     OLED_InitTypeDef oled1;
 
@@ -91,41 +106,80 @@ int main()
     // Device
     Oled1_Init(&oled1);
 
-    SERVO_TypeDef servo1;
-    SERVO_Init(&servo1, RCC_APB1Periph_TIM3, TIM3, 1, RCC_APB2Periph_GPIOA, GPIOA, GPIO_Pin_6);
-    float servoAngle = 0;
-    SERVO_SetAngle(&servo1, servoAngle);
+    // SERVO_TypeDef servo1;
+    // SERVO_Init(&servo1, RCC_APB1Periph_TIM3, TIM3, 1, RCC_APB2Periph_GPIOA, GPIOA, GPIO_Pin_6);
+    // SERVO_SetAngle(&servo1, servoAngle);
+
+    PCA9685_InitTypeDef pca9685;
+    PCA9685_Init(&pca9685, RCC_APB1Periph_I2C2, I2C2, RCC_APB2Periph_GPIOB, GPIOB, GPIO_Pin_10, GPIO_Pin_11);
+
+    float servoAngle[10] = {0};
 
     while (1) {
-        if (KEY_IsPressed(&key1) == 1) {
-            LED_Toggle(&led1);
 
-            if (LED_IsOn(&led1) == 1) {
-                OLED_CleanBuffer(&oled1);
-                // OLED_SetPixel(&oled1, 64, 20, OLED_COLOR_NORMAL);
-                // OLED_DrawLine(&oled1, 0, 0, 127, 63, OLED_COLOR_NORMAL);
-                // OLED_ShowChar(&oled1, 3, 48, 'A', 12, OLED_COLOR_NORMAL);
-                // OLED_ShowChar(&oled1, 30, 48, 'A', 16, OlED_COLOR_REVERSED);
+        uint16_t keyValue = KEY4X4_GetKey(&keyboard);
+        OLED_ShowBinNumber(&oled1, 0, 0, keyValue, 16, OLED_FONT_SIZE_12, OLED_COLOR_NORMAL);
+        OLED_RefreshScreen(&oled1);
+        // if (KEY_IsPressed(&key1) == 1) {
+        //     LED_Toggle(&led1);
 
-                // OLED_ShowString(&oled1, 3, 18, "Abendas", 12, OLED_COLOR_NORMAL);
-                // OLED_ShowString(&oled1, 60, 38, "Abendas", 16, OlED_COLOR_REVERSED);
+        //     if (LED_IsOn(&led1) == 1) {
+        //         OLED_CleanBuffer(&oled1);
+        //         // OLED_SetPixel(&oled1, 64, 20, OLED_COLOR_NORMAL);
+        //         // OLED_DrawLine(&oled1, 0, 0, 127, 63, OLED_COLOR_NORMAL);
+        //         // OLED_ShowChar(&oled1, 3, 48, 'A', 12, OLED_COLOR_NORMAL);
+        //         // OLED_ShowChar(&oled1, 30, 48, 'A', 16, OlED_COLOR_REVERSED);
 
-                // OLED_ShowNumber(&oled1, 70, 1, 1234567, 7, 12, OLED_COLOR_NORMAL);
+        //         // OLED_ShowString(&oled1, 3, 18, "Abendas", 12, OLED_COLOR_NORMAL);
+        //         // OLED_ShowString(&oled1, 60, 38, "Abendas", 16, OlED_COLOR_REVERSED);
 
-                OLED_DrawImage(&oled1, 0, 0, 127, 63, OLED_BMP1);
-                OLED_RefreshScreen(&oled1);
-            } else {
-                OLED_CleanBuffer(&oled1);
-                OLED_RefreshScreen(&oled1);
+        //         // OLED_ShowNumber(&oled1, 70, 1, 1234567, 7, 12, OLED_COLOR_NORMAL);
+
+        //         OLED_DrawImage(&oled1, 0, 0, 127, 63, OLED_BMP1);
+        //         OLED_RefreshScreen(&oled1);
+        //     } else {
+        //         OLED_CleanBuffer(&oled1);
+        //         OLED_RefreshScreen(&oled1);
+        //     }
+
+        //     servoAngle += 30;
+        //     if (servoAngle > 180) {
+        //         servoAngle = 0;
+        //     }
+
+        //     // SERVO_SetAngle(&servo1, servoAngle);
+        //     PCA9685_SetAngle(&pca9685, ch, servoAngle);
+        //     ch++;
+        //     if (ch > 9) {
+        //         ch = 0;
+        //     }
+        // }
+
+        for (uint16_t i = 0; i < 10; i++) {
+            if ((keyValue & (1 << i)) == (1 << i)) {
+                servoAngle[i] += 30;
+                if (servoAngle[i] > 180) {
+                    servoAngle[i] = 0;
+                }
+
+                PCA9685_SetAngle(&pca9685, i, servoAngle[i]);
             }
-
-            servoAngle += 30;
-            if (servoAngle > 180) {
-                servoAngle = 0;
-            }
-
-            SERVO_SetAngle(&servo1, servoAngle);
         }
-        Delay_ms(100);
+
+        if (((keyValue & (1 << 14)) == (1 << 14))) {
+            for (uint16_t i = 0; i < 10; i++) {
+                PCA9685_SetAngle(&pca9685, i, 0);
+                servoAngle[i] = 0;
+                Delay_ms(10);
+            }
+        } else if (((keyValue & (1 << 15)) == (1 << 15))) {
+            for (uint16_t i = 0; i < 10; i++) {
+                PCA9685_SetAngle(&pca9685, i, 180);
+                Delay_ms(10);
+                servoAngle[i] = 180;
+            }
+        }
+
+        Delay_ms(300);
     }
 }
